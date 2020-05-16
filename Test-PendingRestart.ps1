eaf45e067be4e6d86a562e89394a580f55de4342
Dave Evans <dave.evans@goodness.co.uk>
2020-05-16 17:39:23 +0100

    Update Test-PendingRestart.ps1
    fix typo
    write-host reason for restart
    (the first one found)

--- Test-PendingRestart.ps1
+++ Test-PendingRestart.ps1
@@ -15,9 +15,9 @@ Based on:     https://gallery.technet.microsoft.com/scriptcenter/Get-PendingRebo
 #>
 
 function Test-PendingReboot {
-    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
-    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
-    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
+    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { Write-Host "Component Based Servicing\RebootPending"; return $true }
+    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { Write-Host "Auto Update\RebootRequired"; return $true }
+    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { Write-Host "PendingFileRenameOperations"; return $true }
     try { 
         $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
         $status = $util.DetermineIfRebootPending()
@@ -31,7 +31,7 @@ function Test-PendingReboot {
 }
 
 $endresult = Test-PendingReboot
-Write-Host "Pending Rebbot:",  $endresult
+Write-Host "Pending Restart:",  $endresult
 
 If ($endresult) {
     Exit 1
