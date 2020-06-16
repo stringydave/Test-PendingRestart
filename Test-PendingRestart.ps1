306643156a97d7ece84b02ddb804ed507c26b4a1
Dave Evans <dave.evans@goodness.co.uk>
2020-06-16 17:33:15 +0100

    Rename internal function

--- Test-PendingRestart.ps1
+++ Test-PendingRestart.ps1
@@ -5,16 +5,17 @@ Usage: Test-PendingRestart.ps1
 return: 0   no Pending Restart detected
 return: 1   Pending Restart detected
 
-Adapted from: https://stackoverflow.com/questions/47867949/how-can-i-check-for-a-pending-reboot
-Adapted from: https://gist.github.com/altrive/5329377
-Based on:     https://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542 which seems to have moved to: https://github.com/bcwilhite/PendingReboot
+Adapted from:  https://stackoverflow.com/questions/47867949/how-can-i-check-for-a-pending-reboot
+Adapted from:  https://gist.github.com/altrive/5329377
+Based on:      https://github.com/bcwilhite/PendingReboot
+Originally at: https://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542
 
 @author Dave Evans <https://github.com/stringydave>
 @licence MIT
 @version 0.0.1
 #>
 
-function Test-PendingReboot {
+function Test-PendingRestart {
     if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { Write-Host "Component Based Servicing\RebootPending"; return $true }
     if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { Write-Host "Auto Update\RebootRequired"; return $true }
     if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { Write-Host "PendingFileRenameOperations"; return $true }
@@ -30,7 +31,7 @@ function Test-PendingReboot {
     return $false
 }
 
-$endresult = Test-PendingReboot
+$endresult = Test-PendingRestart
 Write-Host "Pending Restart:",  $endresult
 
 If ($endresult) {
